909d95987dc55d45baae76982a0db8cb31e5fe18
Alex <muzo0@ya.ua>
2017-07-26 03:35:58 +0300

    тут подробный текст изменений

--- main.ts
+++ main.ts
@@ -19,11 +19,11 @@ const renderer = new CanvasRenderer(rootNode);
 renderer.setSize(1000, 900);
 
 
-
+// generate
 const circleList = new Map();
 
 const cols = 15;
-const rows = 5;
+const rows = 10;
 for(let i = 1; i <= cols * rows; i++) {
     const circle = new Circle();
     const x = (i % cols) * 60 + 40;
@@ -33,6 +33,7 @@ for(let i = 1; i <= cols * rows; i++) {
     circleList.set(i, circle);
 }
 
+const mousePos = [0, 0];
 
 const getTimeColor = timestamp => {
     const color = Math.round((Math.sin(timestamp / 0xFF5) + 1) / 2 * 358);
@@ -48,12 +49,22 @@ const render = timestamp => {
         const radius = (Math.sin(timestamp / 1000 + i) + 1) * 10 + 10;
         circle.background = getTimeColor(timestamp + i * 1000);
         circle.radius = Math.round(radius);
+
+        if (Math.abs(circle.x - mousePos[0]) + Math.abs(circle.y - mousePos[1]) < 120) {
+            circle.radius += 10;
+        }
+
         renderer.draw(circle);
     });
 
     //stats.end();
     //requestAnimationFrame(render);
 };
+
+document.addEventListener('mousemove', e => {
+    mousePos[0] = e.clientX;
+    mousePos[1] = e.clientY;
+});
 
 setInterval(() => {
     render(Date.now());
