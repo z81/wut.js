2b5ce5d0ab5aff618726c7cae695bbc0e5f13c22
Alex <muzo0@ya.ua>
2017-07-30 17:03:05 +0300

    тут подробный текст изменений

--- src/Plugins/Resizable.ts
+++ src/Plugins/Resizable.ts
@@ -1,29 +1,77 @@
-const resizingElements = new Set();
 const resizeAreaSize = 10;
 
+const DIRECTION = {
+    NONE: 0,
+    LEFT: 2,
+    TOP: 4,
+    RIGHT: 8,
+    BOTTOM: 16
+};
+
+let resizableElement = null;
+let resizeDirection = DIRECTION.NONE;
+let resizeStartPosition = [];
 
 
-const isResizeHandler = (element, x, y) => {
-    let direction = '';
-    if (element.type === 'rect') {
+const getDirection = ({ width, height, x, y, type }, cursorX, cursorY) => {
+    let direction = DIRECTION.NONE;
+    if (type === 'rect') {
+        if (cursorX >= (x + width - resizeAreaSize) && cursorX <= x + width) {
+            direction |= DIRECTION.RIGHT;
+        }
+        else if (cursorX > x && cursorX <= x + resizeAreaSize) {
+            direction |= DIRECTION.LEFT;
+        }
 
-        return (x > element.x + element.width - resizeAreaSize && x < element.x + element.width);
+        if (cursorY >= (y + height - resizeAreaSize) && cursorY <= y + height) {
+            direction |= DIRECTION.BOTTOM;
+        }
+        else if (cursorY >= y && cursorY <= y + resizeAreaSize) {
+            direction |= DIRECTION.TOP;
+        }
     }
 
-    return false;
+    return direction;
 };
 
 
 export function Resizable(element) {
-    element.on('mousemove', (e) => {
-        if (isResizeHandler(element, e.offsetX, e.offsetY)) {
-            element.cursor = 'pointer';
+
+    element.on('mousedown', e => {
+        const direction = getDirection(element, e.offsetX, e.offsetY);
+
+        if (e.buttons > 0 && direction !== DIRECTION.NONE) {
+            resizeDirection = direction;
+            resizableElement = element;
+            resizeStartPosition = [e.offsetX, e.offsetY];
         }
-        else {
-            element.cursor = '';
+    });
+
+    element.on('mousemove', ({ offsetX, offsetY }) => {
+        if (resizableElement !== null) {
+            if (resizeDirection & DIRECTION.LEFT || resizeDirection & DIRECTION.RIGHT) {
+                element.x += offsetX - resizeStartPosition[0];
+                resizeStartPosition[0] = offsetX;
+            }
+
+            return;
         }
 
-        console.log(element.cursor);
+        const direction = getDirection(element, offsetX, offsetY);
+        let cursor = '';
+
+        if (direction & DIRECTION.LEFT) cursor = 'w-resize';
+        if (direction & DIRECTION.RIGHT) cursor = 'w-resize';
+        if (direction & DIRECTION.TOP) cursor = 's-resize';
+        if (direction & DIRECTION.BOTTOM) cursor = 's-resize';
+
+
+        if (direction === DIRECTION.BOTTOM + DIRECTION.LEFT) cursor = 'nesw-resize';
+        if (direction === DIRECTION.BOTTOM + DIRECTION.RIGHT) cursor = 'nwse-resize';
+        if (direction === DIRECTION.TOP + DIRECTION.LEFT) cursor = 'nwse-resize';
+        if (direction === DIRECTION.TOP + DIRECTION.RIGHT) cursor = 'nesw-resize';
+
+        element.cursor = cursor
     });
 
     element.on('mouseleave', (e) => {
